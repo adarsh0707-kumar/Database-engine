98576fcc97a6ea7a1cc5ba05e8b6df2be422a1ed
Adarsh Kumar <adarshku123456789@gmail.com>
2026-05-03 22:04:02 +0530

    store in file

--- src/storage.cpp
+++ src/storage.cpp
@@ -1,24 +1,60 @@
 #include "../include/storage.h"
 #include <iostream>
 #include<map>
+#include <fstream>
 
 static std::map<std::string, std::vector<std::vector<std::string>>> db;
 
+
+std::string get_file_path(const std::string& table) {
+    return "data/" + table + ".table";
+}
+
 void create_table(const std::string& name){
     db[name] = {};
+    
+    std::ofstream file(get_file_path(name));
+
+    file.close();
+
     std::cout<<"Table "<< name<<" created\n";
 }
 
 void insert_into(const std::string& name, const std::vector<std::string>& values){
     db[name].push_back(values);
-    std::cout<<"Inserted into "<< name <<"\n";
+
+    std::ofstream file(get_file_path(name), std::ios::app);
+
+    for(auto& val: values){
+        file << val << " ";
+    }
+
+    file << "\n";
+
+    file.close();
+    
+    std::cout << "Inserted into " << name << "\n";
 }
 
 void select_all(const std::string& name){
-    for(auto& row : db[name]){
-        for(auto& col : row){
-            std::cout << col << " ";
-        }
-        std::cout << "\n";
+    // for(auto& row : db[name]){
+    //     for(auto& col : row){
+    //         std::cout << col << " ";
+    //     }
+    //     std::cout << "\n";
+    // }
+
+    std::ifstream file(get_file_path(name));
+
+    if(!file.is_open()){
+        std::cout << "Table not found\n";
+        return;
     }
+
+    std::string line;
+
+    while(getline(file, line)){
+        std::cout << line << std::endl;
+    }
+    file.close();
 }
