073e83ff4e2864a724fcc77549174f8712a6c757
Adarsh Kumar <adarshku123456789@gmail.com>
2026-05-03 22:39:56 +0530

    parse structured syntax Handle symbols: *, FROM, (, ), ;

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,7 +1,20 @@
 #include "../include/parser.h"
 #include <sstream>
+#include<algorithm>
 
-Command parse_command(const std::string& input){
+
+std::string clean(const std::string& str){
+    std::string s = str;
+    
+    s.erase(remove(s.begin(), s.end(), ';'), s.end());
+    s.erase(remove(s.begin(), s.end(), '('), s.end());
+    s.erase(remove(s.begin(), s.end(), ')'), s.end());
+
+    return s;
+}
+
+Command parse_command(const std::string& input)
+{
     std::stringstream ss(input);
     std::string word;
 
@@ -11,22 +24,30 @@ Command parse_command(const std::string& input){
     ss >> word;
 
     if(word == "CREATE"){
-        std::string tmp;
-        ss >> tmp >> cmd.table;
+        std::string tmp, table;
+
+        ss >> tmp >> table;
         cmd.type = CREATE;
+        cmd.table = clean(table);
     }
     else if(word == "INSERT"){
-        ss >> cmd.table;
+        std::string tem, table, values_word;
+
+        ss >> tem >> table >> values_word;
         cmd.type = INSERT;
+        cmd.table = clean(table);
 
         std::string val;
         while(ss >> val){
-            cmd.values.push_back(val);
+            cmd.values.push_back(clean(val));
         }
     }
     else if(word == "SELECT"){
-        ss >> cmd.table;
+        std::string star, from, table;
+
+        ss >> star >> from >> table;
         cmd.type = SELECT;
+        cmd.table = clean(table);
     }
     return cmd;
 }
